dbbb2b6de87614a3fada7008e1e73f64642c707c
albertofaci <albertofaci@gmail.com>
2018-03-07 22:44:31 +0000

    created speedwords

--- js/common.js
+++ js/common.js
@@ -22,6 +22,24 @@ function displayStartScreen(metadata, onClickCallback) {
 	$('#start').html(metadata.copy.start)
 }
 
+function each(a,f) { for (var i=0,l=a.length;i<l;i++) f(a[i]); }
+
+function scrambleWords(wordList) {
+	w = [];
+	each(wordList, function(word) { w.push(word); });
+	for ( var i=0,l=w.length; i < l; i++ ) {
+		var sindex = Math.floor(Math.random()*l);
+		var temp = w[sindex];
+		w[sindex] = w[i];
+		w[i] = temp;
+	}
+	return w;
+}
+
+function sayWordWithParams(word, locale, speed) {
+	$("#sound").html("<audio src=http://api.voicerss.org/?key=c7390581f1e84a388be14b8cdf507a8f&hl="+locale+"&r="+speed+"&f=44khz_16bit_stereo&ssml=false&src="+encodeURI(word)+" autoplay='autoplay'></audio>")
+}
+
 function pickRandom(num, array) {
 	var selectedElements = []
 	for(var i = 0; i < num; i++) {
